036ea52f270e8371be8c44437cb1c101da85776c
DarKS4ys <melihyardim1057@gmail.com>
2024-04-24 00:44:25 +0300

    a

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -4,13 +4,13 @@ import { currentUser } from '@clerk/nextjs/server';
 import Image from 'next/image';
 
 export default async function Home() {
-  const users = await db.user.findMany({
+/*   const users = await db.user.findMany({
     where: { status: 'Admin' },
     orderBy: { percentage: 'asc' },
     include: {HistoryItem: true}
-  });
+  }); */
 
- /*  const currUser = await currentUser() */
+  const currUser = await currentUser()
   return (
     <main className="h-screen flex flex-col gap-y-8 justify-center items-center max-w-6xl mx-auto">
       <h1 className="text-5xl font-semibold uppercase">Slack Board</h1>
